126b55f00929cc9758d62d17487bef6e359cd8b8
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-30 06:21:00 +0530

    Refactor

--- src/components/UseStates/Counter.tsx
+++ src/components/UseStates/Counter.tsx
@@ -9,7 +9,7 @@ const Counter = () => {
 
   return (
     <div>
-      <p>Count: {count}</p>
+      <p className="text-2xl font-bold mb-2 text-gray-200">Count: {count}</p>
       <button
         className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out"
         onClick={incrementCount}
